ecf1245a85b645decc7695c994130488b0527ace
Tim Hallberg <tim.hallberg@outlook.com>
2022-12-06 14:38:14 +0100

    Felicias Meny

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,1 +1,66 @@
 import "./Menu.css";
+import { NavLink } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { NavigationData } from '../../NavigationData';
+import { IconContext } from 'react-icons';
+
+//Felicia
+
+
+function Navigation() {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+    let handler = (e)=>{
+      if(!menuRef.current.contains(e.target)){
+        setSidebar(false);
+      }      
+    };
+
+    document.addEventListener("mousedown", handler);
+    
+
+    return() =>{
+      document.removeEventListener("mousedown", handler);
+    }
+
+  });
+
+  return (
+    <div ref={menuRef}>
+      <IconContext.Provider value={{ color: '#faebd7' }}>
+        <div className='navbar'>
+          <NavLink to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </NavLink>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <NavLink to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </NavLink>
+            </li>
+            {NavigationData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <NavLink to={item.path}>
+                    <span>{item.title}</span>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </div>
+  );
+}
+
+export default Navigation;
---
+++ src/NavigationData.jsx
@@ -0,0 +1,37 @@
+export const NavigationData = [
+    {
+      title: 'Home',
+      path: '/',
+      cName: 'nav-text'
+    },
+    {
+      title: 'Smältande Glaciärer',
+      path: '/smaltande-glaciarer',
+      cName: 'nav-text'
+    },
+    {
+      title: 'Stigande Havsnivåer',
+      path: '/stigande-havsnivaer',
+      cName: 'nav-text'
+    },
+    {
+      title: 'Co2 Utsläpp',
+      path: '/co2-utslapp',
+      cName: 'nav-text'
+    },
+    {
+      title: 'Global Uppvärmning',
+      path: '/global-uppvarming',
+      cName: 'nav-text'
+    },
+    {
+      title: 'Vad kan jag göra?',
+      path: '/tips',
+      cName: 'nav-text'
+    },
+    {
+        title: 'Vad kan jag om klimatförändringarna?',
+        path: '/fragor',
+        cName: 'nav-text'
+    }
+  ];
