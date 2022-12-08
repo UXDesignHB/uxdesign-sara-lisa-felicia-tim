5c3d05dfa9a46859c6b0b7b257f769573aa1f2ec
Tim Hallberg <tim.hallberg@outlook.com>
2022-12-08 14:54:04 +0100

    Theme with graph

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)",
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#EC8394",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit.",
   },
   {
     id: "2",
@@ -25,7 +27,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)",
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#B29BCA",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit."
   },
   {
     id: "3",
@@ -33,7 +37,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)", 
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#A8DAEA",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit."
   },
   {
     id: "4",
@@ -41,7 +47,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)",
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#F1C454",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit."
   
   },
   {
@@ -50,7 +58,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)",
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#A8DAEA",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit."
   },
   {
     id: "6",  
@@ -58,7 +68,9 @@ const data = [
     subtitle: "TEMPERATURFÖRÄNDRINGAR (1880-2016)",
     graphtitle: "NU ÄR DET VARMARE ÄN NÅGONSIN",
     textcolor: "#A8DAEA",
-    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin."
+    graphtext: "Jordens temperatur har stigit med 0,14 ° Fahrenheit (0,08 ° Celsius) per decennium sedan 1880, men uppvärmningshastigheten sedan 1981 är mer än dubbelt så stor: 0,32 ° F (0,18 ° C) per decennium. År 2021 var det sjätte varmaste året någonsin baserat på temperaturdata. I genomsnitt över land och hav var yttemperaturen 2021 1,51 °F (0,84 °C) varmare än 1900-talets genomsnitt på 57,0 °F (13,9 °C) och 1,87 ˚F (1,04 ˚C) varmare än den förindustriella period (1880-1900). De nio åren från 2013 till 2021 rankas bland de 10 varmaste åren någonsin.",
+    videotitle: "VISUALITION ÖVER GLOBALA TEMPERATURAVVIKELSER",
+    videotext: "Denna färgkodade karta i Robinson-projektion visar en utveckling av förändrade globala yttemperatur-avvikelser. Normala temperaturer visas i vitt. Högre än normala temperaturer visas i rött och lägre än normala temperaturer visas i blått. Normala temperaturer beräknas över den 30-åriga baslinjeperioden 1951-1980. Den sista ramen representerar de 5-åriga globala temperaturavvikelserna från 2017-2021. Skala i grader Fahrenheit."
   },
 ];
 
