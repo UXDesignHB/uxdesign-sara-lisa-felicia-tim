e76802dd71aea6843178f7396e9238c58929fe9d
sarabiverstedt <sarabiverstedt@hotmail.com>
2022-12-08 14:51:51 +0100

    Dark mode

--- src/Components/Artic/Artic.jsx
+++ src/Components/Artic/Artic.jsx
@@ -12,21 +12,21 @@ const Artic = () => {
 
     
             <div className="articSection">
-            <div className="articButtons">
-       <Link to="/smaltande-glaciarer"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">Smältande glaciärer</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div> </Link>
-       <Link to="/stigande-havsnivaer"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">Stigande havsnivåer</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div></Link>
-       <Link to="/co2-utslapp"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">CO2-utsläpp</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div></Link>
-       <Link to="/global-uppvarming"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">Global uppvärmning</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div></Link>
-       <Link to="/tips"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">Vad kan jag göra?</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div></Link>
-       <Link to="/fragor"> <div className="articButton"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading">Vad kan jag om klimatförändringarna?</h3><BsArrowUp className="arrowIcon"/><p>Lorem impsum</p></div></Link>
-    </div>
+        
             <div className="articVideo">
-                <h2></h2>
-                <p></p>
+                <h2>Lorem impsum</h2>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias libero eligendi eveniet nihil perspiciatis facere ipsam. Nesciunt eum debitis unde magni, minima ut ex optio est architecto quibusdam eaque pariatur?</p>
                 <iframe src="https://youtu.be/H71LG6xgZII" frameborder="0"></iframe>
             </div>
 <div className="articImage">
- 
+<div className="articButtons">
+       <Link to="/smaltande-glaciarer"> <div className="articButton articButton1"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading1">Smältande glaciärer</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div> </Link>
+       <Link to="/stigande-havsnivaer"> <div className="articButton articButton2"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading2">Stigande havsnivåer</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div></Link>
+       <Link to="/co2-utslapp"> <div className="articButton articButton3"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading3">CO2-utsläpp</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div></Link>
+       <Link to="/global-uppvarming"> <div className="articButton articButto4"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading4">Global uppvärmning</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div></Link>
+       <Link to="/tips"> <div className="articButton articButton5"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading5">Vad kan jag göra?</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div></Link>
+       <Link to="/fragor"> <div className="articButton articButton6"><BsPlusCircle className="plusIcon"/><h3 className="smallHeading articButtonHeading6">Vad kan jag om klimatförändringarna?</h3><BsArrowUp className="arrowIcon"/><div className="articParagraph"><p className="articParagraph1">Lorem impsum</p><sup className="articParagraph2">Lorem impsum</sup></div></div></Link>
+    </div>
 <img className="glacier bear" src={Bear}/>
             <img className="glacier ice" src={Glacier}/>
             </div>
--- src/Components/Tips/Tips.jsx
+++ src/Components/Tips/Tips.jsx
@@ -1,28 +1,81 @@
 import "./Tips.css";
 
-
 const Tips = () => {
-    return ( 
-        <div className="container">
+  return (
+    <div className="container">
+      <header>
+        <h1 className="bigHeading tipsBigHeading">Enkla tips</h1>
 
-        <header>
-            <h1 className="bigHeading">Rubrik</h1>
+        <div className="headerButtons">
 
-            <div className="headerButtons">
+          <button className="closeButton">X</button>
+        </div>
+      </header>
+      
 
-                <button>Dark mode</button>
+      <p className="mediumHeading">
+        Hur kan du bidra till att bekämpa klimatförändringarna?
+      </p>
 
-                <button className="closeButton">X</button>
-            </div>
+      <div className="outerContainer">
+        <div className="innerContainer" id="item1">
+          <p className="smallHeading">Minska på köttet</p>
+          <div className="line"></div>
+          <p>
+            {" "}
+            Utsläpp från boskap står för 14,5% av alla växthusgaser. Genom att
+            minska konsumtionen av kött.....{" "}
+          </p>
+        </div>
 
-        </header>
-        <p className="mediumHeading">Detta är tips komponenten</p>
-        <p className="smallHeading">Detta är tips komponenten</p>
+        <div className="innerContainer" id="item2">
+          <p className="smallHeading">Undvik bilen</p>
+          <div className="line"></div>
+          <p>
+            {" "}
+            Cykla, gå eller använd kollektivtrafiken. Minska antalet bilresor
+            genom att endast använda bilen...
+          </p>
+        </div>
 
+        <div className="innerContainer" id="item3">
+          <p className="smallHeading">Börja källsortera</p>
+          <div className="line"></div>
+          <p>
+            Sätt upp speciella kärl för att källsortera skräp hemma och i
+            skolan. När du källsorterar ditt skräp kan det återvinnas...{" "}
+          </p>
+        </div>
+
+        <div className="innerContainer" id="item4">
+          <p className="smallHeading">Börja källsortera</p>
+          <div className="line"></div>
+          <p>
+            Sätt upp speciella kärl för att källsortera skräp hemma och i
+            skolan. När du källsorterar ditt skräp kan det återvinnas...{" "}
+          </p>
+        </div>
+
+        <div className="innerContainer" id="item5">
+          <p className="smallHeading">Börja källsortera</p>
+          <div className="line"></div>
+          <p>
+            Sätt upp speciella kärl för att källsortera skräp hemma och i
+            skolan. När du källsorterar ditt skräp kan det återvinnas...{" "}
+          </p>
+        </div>
+
+        <div className="innerContainer" id="item6">
+          <p className="smallHeading">Börja källsortera</p>
+          <div className="line"></div>
+          <p>
+            Sätt upp speciella kärl för att källsortera skräp hemma och i
+            skolan. När du källsorterar ditt skräp kan det återvinnas...{" "}
+          </p>
+        </div>
+      </div>
     </div>
-     );
-}
- 
+  );
+};
+
 export default Tips;
-
-
