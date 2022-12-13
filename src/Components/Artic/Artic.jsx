b8c262df6a8d06a86528ddc5fc4e2c64b2ce1d17
sarabiverstedt <sarabiverstedt@hotmail.com>
2022-12-13 18:56:47 +0100

    Footer

--- src/Components/Artic/Artic.jsx
+++ src/Components/Artic/Artic.jsx
@@ -1,7 +1,6 @@
 import "./Artic.css";
 import Glacier from "../../Images/Glacier.png";
 import Bear from "../../Images/Bear.png";
-import { BsArrowUp, BsParagraph, BsPlusCircle } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { ButtonData } from '../../ButtonData';
 import ReactPlayer from "react-player";
@@ -21,6 +20,8 @@ const Artic = () => {
   <div className="articSection">
 
     <div className="articVideo">
+
+
     <ReactPlayer
                       url= "https://youtu.be/H71LG6xgZII"
                       className="articYT"
@@ -30,30 +31,29 @@ const Artic = () => {
                     //   muted={true}
                       playsinline={true}
                       onReady={onLoadedData}
-                      width="100%"
-                      height="200px"
+                      width="auto"
+                      height="16rem"
                     />
-
-        <div>
-            <h2 className="mediumHeading">Klimatförändringarna</h2>
-            <p>
+            <p className="largeText">
             Klimatförändringar syftar på de många olika effekter som den globala uppvärmning
             har på jordens klimatsystem. Hit hör bland annat stigande havsnivåer, smältande
             glaciärer, förändrade regnmönster och CO2-utsläpp från fossila bränslen. Läs mer
             om hur det påverkar vår jord och hur du kan påverka.
             </p>
-        </div>
 
-  
+
+
+      
     </div>
 
     <div className="articImage">
         <div className="articButtons">
 
         {ButtonData.map((button) => (
-                <Link to={button.link}>
+                <Link to={button.link}>   
                 <div className="articButton">  <div className="plus icon"></div>
-                    <h3 className="smallHeading buttonHeading">{button.heading}</h3><BsArrowUp className="arrowIcon"/>
+                    <h3 className="smallHeading buttonHeading">{button.heading}</h3>
+                    {button.arrow}
                     <div className="articParagraph">
                         <p className="articParagraph1">{button.paragraph1}</p>
                         <sup className="articParagraph2">{button.paragrap2}</sup>
